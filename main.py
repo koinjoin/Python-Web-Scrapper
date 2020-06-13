0b265789b3ab599c00a5dc38d96574b2574cbe4b
dev24day <kjyanus@gmail.com>
2020-06-13 19:21:54 +0900

    extract data w/ beautifulsoup

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-result_indeed = requests.get('https://www.indeed.com/jobs?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&as_src&salary&radius=25&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=aeed01dbd68defa8', auth=('user', 'pass'))
-print(result_indeed.status_code)
-print(result_indeed.headers['content-type'])
-print(type(result_indeed))
-print(result_indeed)
-print(result_indeed.text)
+indeed = requests.get('https://www.indeed.com/jobs?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&as_src&salary&radius=25&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=aeed01dbd68defa8', auth=('user', 'pass'))
+# print(indeed.status_code)
+# print(indeed.headers['content-type'])
+# print(type(indeed))
+# print(indeed)
+# print(indeed.text) 
+# all the html...
+
+res_indeed = indeed.text
+soup_indeed = BeautifulSoup(res_indeed, 'html.parser')
+print(soup_indeed.title)
+print(soup_indeed.title.string)
