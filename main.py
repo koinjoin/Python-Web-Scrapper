6cf8aad1ad8be8a32979b7835f52c21c683e2da6
dev24day <kjyanus@gmail.com>
2020-06-16 18:11:20 +0900

    comment delete

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 indeed = requests.get('https://www.indeed.com/jobs?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&as_src&salary&radius=25&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=aeed01dbd68defa8', auth=('user', 'pass'))
-# print(indeed.status_code)
-# print(indeed.headers['content-type'])
-# print(type(indeed))
-# print(indeed)
-# print(indeed.text) 
-# all the html...
 
 indeed_html = indeed.text
 indeed_soup = BeautifulSoup(indeed_html, 'html.parser')
