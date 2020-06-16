efd1dfc7b8f470b24c5e44f99e640f02b5f16ac2
dev24day <kjyanus@gmail.com>
2020-06-16 18:33:42 +0900

    extract for n times

---
+++ indeed.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+URL = f'https://www.indeed.com/jobs?q=python&limit={LIMIT}'
+
+def extractPages():
+    response = requests.get(URL, auth=('user', 'pass'))
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    pagination = soup.find('ul', {"class":"pagination-list"}) 
+    pageLink = pagination.find_all('a')
+    pages = []
+    for link in pageLink[:-1]:
+        pages.append(int(link.string))
+    last_page = pages[-1]
+    return last_page
+    
+def extractJobs(last_page):
+    for page in range(last_page):
+        print(f'start={page*50}')
+
--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
-
-indeed = requests.get('https://www.indeed.com/jobs?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&as_src&salary&radius=25&l&fromage=any&limit=50&sort&psf=advsrch&from=advancedsearch&vjk=aeed01dbd68defa8', auth=('user', 'pass'))
-
-indeed_html = indeed.text
-indeed_soup = BeautifulSoup(indeed_html, 'html.parser')
-indeed_pagination = indeed_soup.find('ul', {"class":"pagination-list"}) 
-indeed_pageLink = indeed_pagination.find_all('a')
-pages = []
-for link in indeed_pageLink[:-1]:
-    pages.append(int(link.string))
-indeed_maxPage = pages[-1]
+import indeed
 
 
+indeed.extractJobs(indeed.extractPages())
