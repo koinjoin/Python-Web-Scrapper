a208f73994e73c67a549e2282cd893da19a0ea51
dev24day <kjyanus@gmail.com>
2020-06-27 14:22:47 +0900

    Install flask

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import indeed as ind
 import stackoverflow as stf
 import save
+from flask import Flask
 
-
-ind_jobs = ind.startScrap()
+# ind_jobs = ind.startScrap()
 # stf_jobs = stf.startScrap()
-save.save_to_file(ind_jobs, 'ind_jobs')
+# save.save_to_file(ind_jobs, 'ind_jobs')
 # save.save_to_file(stf_jobs, 'stf_jobs')
+
+app = Flask("myScrapper")
+
+@app.route("/")
+def home():
+    return "Hello People"
+
+app.run()
