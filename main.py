6b3268b7684e2962199ff22d49d1b49bfe41cfdd
dev24day <kjyanus@gmail.com>
2020-06-27 20:31:34 +0900

    formating ..

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ from flask import Flask, render_template, request
 
 app = Flask("myScrapper")
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/result")
 def result():
-    searchedJob = request.args.get('job')
-    
-    return render_template("result.html",searched=searchedJob) #인자를 넘겨서 템플릿을 구성 => soSexxxy
+    searchedJob = request.args.get("job")
+
+    return render_template(
+        "result.html", searched=searchedJob
+    )  # 인자를 넘겨서 템플릿을 구성 => soSexxxy
+
 
 print(__name__)
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-
-    
